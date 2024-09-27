cb51f880a5fe12a8b6871ed507b8aee9acbc4f99
rabbitglauser <samuel.glauser@gmail.com>
2024-09-27 08:41:25 +0200

    Add new classes and fix property access in proxies
    Introduced `InventoryManagementSystem` and `calculator` classes to demonstrate OOP principles in JavaScript. Fixed an error in proxy property access log by correcting index from 4 to 3.

--- src/calculator.js
+++ src/calculator.js
@@ -1,18 +1,33 @@
 // learning how to create a calculator in javascript but using OOP
 
-class calculator{
-    constructor(addition, subtraction, multiplication, division ) {
-    this.addition  = addition
-    this.subtraction  = subtraction
-    this.multiplication = multiplication
-    this.division = divison
+class Calculator {
+    constructor() {
     }
 
-    calculation(){
+    addition(number1, number2) {
+        const additionCalculation = number1 + number2;
+        return additionCalculation;
     }
 
+    subtraction(number1, number2) {
+        return number1 - number2;
+    }
 
+    multiplication(number1, number2) {
+        return number1 * number2;
+    }
+
+    division(number1, number2) {
+        if (number2 === 0) {
+            throw new Error("Division by zero is not allowed.");
+        }
+        return number1 / number2;
+    }
 }
 
+const calc = new Calculator();
 
-
+console.log(calc.addition(3, 5)); // Output: 8
+console.log(calc.subtraction(10, 4)); // Output: 6
+console.log(calc.multiplication(6, 7)); // Output: 42
+console.log(calc.division(20, 5)); // Output: 4
